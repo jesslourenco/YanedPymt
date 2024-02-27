d70bea814c5e17420d7f2786eb78cd3cf4d7abd5
jesslourenco <70455379+jesslourenco@users.noreply.github.com>
2024-02-27 17:59:28 -0500

    docker & flyway setup

--- yanedpayments/src/main/java/com/evilcorp/yanedpayments/entity/Account.java
+++ yanedpayments/src/main/java/com/evilcorp/yanedpayments/entity/Account.java
@@ -1,22 +1,13 @@
 package com.evilcorp.yanedpayments.entity;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.math.BigDecimal;
 
-@Data
-@Entity
+
 public class Account {
-    @Id
-    @SequenceGenerator(
-            name = "account_id_sequence",
-            sequenceName = "account_id_sequence"
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "account_id_sequence"
-    )
-    private long id;
+    private int id;
     private BigDecimal balance;
+
+    public Account(BigDecimal balance){
+        this.balance = BigDecimal.ZERO;
+    }
 }
